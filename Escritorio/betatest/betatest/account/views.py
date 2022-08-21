e30979d022f0ad07b23f071e687fe23c43bde334
Matias <sanchezmatias968@gmail.com>
2022-08-21 19:00:53 -0400

    first commit

--- Escritorio/betatest/betatest/account/views.py
+++ Escritorio/betatest/betatest/account/views.py
@@ -8,20 +8,3 @@ from django.contrib.auth.decorators import login_required
 def tablero(request):
     return render(request,'account/tablero.html', {'section':'tablero'})
 
-def user_login(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(request,username=cd['username'],password=cd['password'])
-            if user is not None:
-                if user.is_active:
-                    login(request,user)
-                    return HttpResponse('Autenticado'' Satisfactoriamente')
-                else:
-                    return HttpResponse('Cuenta desabilitada')
-            else:
-                return HttpResponse("Login Invalido")
-    else:
-        form=LoginForm()
-    return render(request,'account/login.html',{'form':form})
--- Escritorio/betatest/betatest/betatest/urls.py
+++ Escritorio/betatest/betatest/betatest/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('account.urls')),
+
     path('account/',include('account.urls')),
     path('social-auth',include('social_django.urls',namespace='social')),
 
